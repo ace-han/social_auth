64858a6d55ac120f45e6421a5aa94f46d003eefc
Ace <ace.jl.han@gmail.com>
2021-08-23 00:33:03 +0800

    patch sessions app to save session in db

--- social_auth/apps.py
+++ social_auth/apps.py
@@ -11,19 +11,20 @@ class SocialAuthConfig(AppConfig):
     verbose_name = "Social Auth"
 
     def ready(self):
-        patch_app_name = 'social_django'
-        patch_app_installed = apps.is_installed(patch_app_name)
+        patch_app_names = ('social_django', 'django.contrib.sessions', )
+        patch_app_installed = apps.is_installed(patch_app_names[0])
 
         if patch_app_installed:
-            logger.info('SocialAuthConfig.ready, app: %s, is_installed: %s', patch_app_name, patch_app_installed)
+            logger.info('SocialAuthConfig.ready, app: %s, is_installed: %s', patch_app_names[0], patch_app_installed)
         else:
-            logger.info('SocialAuthConfig.ready, app: %s, is_installed: %s, doing initialization...', patch_app_name, patch_app_installed)
+            logger.info('SocialAuthConfig.ready, app: %s, is_installed: %s, doing initialization...', patch_app_names[0], patch_app_installed)
             # refer to
             # https://stackoverflow.com/questions/24027901/dynamically-loading-django-apps-at-runtime#answer-57897422
-            settings.INSTALLED_APPS += (patch_app_name, )
+            settings.INSTALLED_APPS += patch_app_names
             apps.app_configs = OrderedDict()
             apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
             apps.clear_cache()
             apps.populate(settings.INSTALLED_APPS)
-            # management.call_command('makemigrations', new_app_name, interactive=False)
-            management.call_command('migrate', patch_app_name, interactive=False)
+            for patch_app_name in patch_app_names:
+                management.call_command('migrate', patch_app_name.rsplit('.', maxsplit=1)[-1], interactive=False)
+            # management.call_command('migrate', interactive=False)
