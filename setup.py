43ccb12555543379e77bb7850e4b920a8ab72c01
Ace <ace.jl.han@gmail.com>
2021-08-23 23:01:51 +0800

    ref #3, add `extras_require` for this project

--- setup.py
+++ setup.py
@@ -4,12 +4,25 @@ install_requires = [
     'social-auth-app-django',
 ]
 
-setup(
+# copy from `python-social-auth` social-core/setup.py
+social_core_extras_require={
+    'openidconnect': ['python-jose>=3.0.0'],
+    'saml': ['python3-saml>=1.2.1'],
+    'azuread': ['cryptography>=2.1.1'],
+    'all': [
+        'python-jose>=3.0.0', 
+        'python3-saml>=1.2.1', 
+        'cryptography>=2.1.1',
+    ]
+}
+
+setu(
     name="social_auth",
     version="0.1.0",
     packages=["social_auth"],
     package_dir={"social_auth": "social_auth"},
     install_requires=install_requires,
+    extras_require=social_core_extras_require,
     entry_points={
         "saleor.plugins": ["social_auth = social_auth.plugin:SocialAuthPlugin"]
     },
